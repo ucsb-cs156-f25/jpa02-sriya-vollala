7e2f842b6a430dee28132bc00a0864f9f76c3b1b
sriya-vollala <sriya.vollala@gmail.com>
2025-10-08 00:46:21 -0700

    all new changes

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -39,14 +39,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("f25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f25-08");
+        team.addMember("Sriya V.");
+        team.addMember("Lucy");
+        team.addMember("Dalia");
+        team.addMember("Emily");
+        team.addMember("Varsha");
+        team.addMember("Krystelle");
         return team;
     }
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -31,6 +31,23 @@ public class DeveloperTest {
         assertEquals("sriya-vollala", Developer.getGithubId());
     }
 
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("f25-08", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Sriya V."),"Team should contain Sriya V.");
+        assertTrue(t.getMembers().contains("Lucy"),"Team should contain Lucy");
+        assertTrue(t.getMembers().contains("Dalia"),"Team should contain Dalia");
+        assertTrue(t.getMembers().contains("Emily"),"Team should contain Emily");
+        assertTrue(t.getMembers().contains("Varsha"),"Team should contain Varsha");
+        assertTrue(t.getMembers().contains("Krystelle"),"Team should contain Krystelle");
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
